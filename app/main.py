4cb6b7236f2cbb1806736d7d533f6b512b654b1b
MinaAlmasi <mina@cc.au.dk>
2026-04-14 22:02:47 +0200

    add allowed origins (local host for now)

--- app/main.py
+++ app/main.py
@@ -3,12 +3,23 @@ Init FastAPI
 """
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.chat import router as chat_router
 
 
 def create_app() -> FastAPI:
     app = FastAPI()
+
+    origins = ["http://localhost:3000"]
+
+    # add CORS 
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     # add chat router (more routers added later)
     app.include_router(chat_router)
