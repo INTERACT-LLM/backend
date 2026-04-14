28adedcf1c267b0f4e75409f08c689ec194fc14f
MinaAlmasi <mina@cc.au.dk>
2026-04-14 12:59:03 +0200

    return a data model

--- app/api/chat.py
+++ app/api/chat.py
@@ -1,15 +1,17 @@
 """
 Add request and response handling here
 """
-from app.services.llm import generate_reply
+
 from fastapi import APIRouter
+
 from app.models.chat import ChatMessage
+from app.services.llm import generate_reply
 
 router = APIRouter()
+
 
 @router.post("/chat")
 async def chat(request: ChatMessage):
     generated_reply = generate_reply(request.message)
 
-    return generated_reply
-
+    return ChatMessage(message=generated_reply, role="assistant")
